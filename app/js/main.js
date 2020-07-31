71ad7c6a86e9a1f45af9ec8c2f8037d3b51aa587
Yigal Igor Maksimov <yigal.maksimov@gmail.com>
2020-08-01 02:10:28 +0300

    adaptive site and scroll menu and scroll up buttom

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,27 @@ $(function () {
     arrows: false,
     autoplay: true,
   });
+  $(".burger__menu").on("click", function () {
+    $(".burger__menu,.header__nav-menu").toggleClass("active");
+    $("body").toggleClass("block");
+  });
+  $("#menu").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr("href"),
+      top = $(id).offset().top;
+    $("body,html").animate({ scrollTop: top }, 1500);
+    $("body").removeClass("block");
+    $(".burger__menu,.header__nav-menu").removeClass("active");
+  });
+  $(window).scroll(function () {
+    if ($(this).scrollTop() != 0) {
+      $("#toTop").fadeIn();
+    } else {
+      $("#toTop").fadeOut();
+    }
+  });
+
+  $("#toTop").click(function () {
+    $("body,html").animate({ scrollTop: 0 }, 800);
+  });
 });
